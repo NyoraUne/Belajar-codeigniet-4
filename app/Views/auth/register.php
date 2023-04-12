dee3024ae86dbebcf39bfd1a10d0f986e5070538
anderiyan <andriyann77@gmail.com>
2023-04-12 11:22:29 +0800

    Pembuatan Form Register Fix

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -21,6 +21,22 @@
 </head>
 
 <body class="bg-gradient-primary">
+    <!-- validatinon -->
+    <?php
+    $session = session();
+    $error = $session->getFlashdata('error');
+    ?>
+
+    <?php if ($error) { ?>
+        <p style="color:red">Terjadi Kesalahan:
+        <ul>
+            <?php foreach ($error as $e) { ?>
+                <li><?php echo $e ?></li>
+            <?php } ?>
+        </ul>
+        </p>
+    <?php } ?>
+    <!-- validatinon -->
 
     <div class="col-xl-6 container">
 
@@ -33,27 +49,30 @@
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                             </div>
-                            <form class="user">
+
+                            <!-- start form -->
+                            <form class="user" method="post" action="/auth/valid_register">
 
                                 <div class="form-group">
-                                    <input type="text" class="form-control form-control-user" id="exampleInputEmail" placeholder="Username">
+                                    <input name="nama" type="text" class="form-control form-control-user" id="exampleInputEmail" placeholder="Username">
                                 </div>
                                 <div class="form-group">
-                                    <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
+                                    <input name="email" type="text" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
                                 </div>
                                 <div class="form-group row">
                                     <div class="col-sm-6 mb-3 mb-sm-0">
-                                        <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                                        <input name="password" type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
                                     </div>
                                     <div class="col-sm-6">
-                                        <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password">
+                                        <input name="confirm" type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password">
                                     </div>
                                 </div>
-                                <button type="submit" class="btn btn-primary btn-user btn-block">
+                                <button name="login" type="submit" class="btn btn-primary btn-user btn-block">
                                     Register Account
                                 </button>
-
                             </form>
+                            <!-- end form -->
+
                             <hr>
                             <div class="text-center">
                                 <a class="small" href="<?= base_url('aurh/login') ?>">Already have an account? Login!</a>
