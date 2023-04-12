5e6885228bd588f105fe454dc45ac9c3f8aa2b61
anderiyan <andriyann77@gmail.com>
2023-04-12 13:17:28 +0800

    Pembuatan Menu Utama Admin Index fix

--- app/Views/admin/head.php
+++ app/Views/admin/head.php
@@ -2,17 +2,12 @@
 <html lang="en">
 
 <head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
     <title>SB Admin 2 - Blank</title>
 
     <!-- Custom fonts for this template-->
-    <link href="<?= base_url('assets/') ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <!-- <link href="<?= base_url('assets/') ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css"> -->
+    <!-- <link href="<?= base_url('assets/') ?>css/all.css" rel="stylesheet" type="text/css"> -->
+    <script src="https://kit.fontawesome.com/2d2eae609e.js" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
@@ -43,7 +38,7 @@
             <!-- Nav Item - Dashboard -->
             <li class="nav-item">
                 <a class="nav-link" href="index.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <i class="fa-solid fa-money-bill-transfer"></i>
                     <span>Dashboard</span></a>
             </li>
 
@@ -58,14 +53,14 @@
             <!-- Nav Item - Pages Collapse Menu -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
+                    <i class="fa-sharp fa-solid fa-boxes-packing"></i>
+                    <span>Barang Dan Jasa</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
+                        <h6 class="collapse-header">Data Barang Dan Jasa:</h6>
+                        <a class="collapse-item" href="buttons.html">Data Barang</a>
+                        <a class="collapse-item" href="cards.html">Data Jasa</a>
                     </div>
                 </div>
             </li>
